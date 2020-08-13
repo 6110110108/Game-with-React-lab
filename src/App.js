507a7138bf5d142e0e19c878d3e671cc80537814
SEOULCHA RATMUMAD <6110110108@psu.ac.th>
2020-08-14 01:31:40 +0700

    Test random

--- src/App.js
+++ src/App.js
@@ -2,15 +2,87 @@ import React from 'react';
 import './App.css';
 import WordCard from './WordCard';
 
-const word = "Hello";
+const word1 = "Hello";
+const word2 = "Smile";
+const word3 = "Image";
+const word4 = "Engine";
+const word5 = "ability";
+
+function App() {
+  var x = Math.floor(Math.random() * 4);
+  if(x == 0) {
+    return (
+      <div className="App">
+            {
+              <WordCard value={word1}/>
+            }
+      </div>
+    );
+  }
+  if(x == 1) {
+    return (
+      <div className="App">
+            {
+              <WordCard value={word2}/>
+            }
+      </div>
+    );
+  }
+  if(x == 2) {
+    return (
+      <div className="App">
+            {
+              <WordCard value={word3}/>
+            }
+      </div>
+    );
+  }
+  if(x == 3) {
+    return (
+      <div className="App">
+            {
+              <WordCard value={word4}/>
+            }
+      </div>
+    );
+  }
+  if(x == 4) {
+    return (
+      <div className="App">
+            {
+              <WordCard value={word5}/>
+            }
+      </div>
+    );
+  }
+}
+
+export default App;
+
+/*import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
   return (
     <div className="App">
-        {
-          <WordCard value={word}/>
-        } 
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          ABC order by clicking Game
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
 
-export default App;
+export default App;*/
+
