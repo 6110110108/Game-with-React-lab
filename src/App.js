ff321014486530aa7285645a121fb244207cb50b
SEOULCHA RATMUMAD <6110110108@psu.ac.th>
2020-08-15 02:53:57 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,39 +7,17 @@ var word = "";
 var rand = Math.floor(Math.random() * 11);
 
 switch (rand) {
-  case 0:
-    word = "Hello";
-    break;
-  case 1:
-    word = "Smile";
-    break;
-  case 2:
-    word = "Image";
-    break;
-  case 3:
-    word = "Engine";
-    break;
-  case 4:
-    word = "Signal";
-    break;
-  case 5:
-    word = "Apple";
-    break;
-  case 6:
-    word = "Market";
-    break;
-  case 7:
-    word = "Vanilla";
-    break;
-  case 8:
-    word = "Laptop";
-    break;
-  case 9:
-    word = "Digital";
-    break;
-  case 10:
-    word = "Computer";
-    break;
+  case 0: word = "Hello"; break;
+  case 1: word = "Smile"; break;
+  case 2: word = "Image"; break;
+  case 3: word = "Engine"; break;
+  case 4: word = "Signal"; break;
+  case 5: word = "Apple"; break;
+  case 6: word = "Market"; break;
+  case 7: word = "Vanilla"; break;
+  case 8: word = "Laptop"; break;
+  case 9: word = "Digital"; break;
+  case 10: word = "Computer"; break;
 }
 
 function App() {
